73a13a28c4ad0f50e6c32cea36c76363f0b1a9d8
Hanson Pu <hanson.pu@kaiostech.com>
2017-03-17 22:04:08 +0800

    iOS HelloWorld template

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Hanson Pu on 2017/3/4.
+//  Copyright © 2017年 Hanson Pu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //@IBOutlet var myWebView:UIWebView
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //myWebView.loadRequest(NSURLRequest(URL:NSURL(string:"http:tw.google.com")));
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
